d5dd8f579e29b14b0a6464493b44ddf6871a42f9
Languini <popovici123alex75@gmail.com>
2026-07-29 20:47:48 +0300

    Adaugare model evaluation pentru un model si model comparison pentru 4 modele

---
+++ src/model_comparison.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import os
+from data_preprocessing import split_features_and_target
+from sklearn.model_selection import train_test_split
+import joblib
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+
+DATA_PATH = "data/features_data.csv"
+MODEL_PATH = "models/"
+
+df = pd.read_csv(DATA_PATH)
+
+X, Y = split_features_and_target(df)
+X_train, X_test, Y_train, Y_test =train_test_split(X, Y, test_size = 0.2, random_state=42)
+
+rezultate = []
+
+for file in os.listdir(MODEL_PATH):
+    CURRENT_MODEL_PATH = f"models/{file}"
+    
+    model = joblib.load(CURRENT_MODEL_PATH)
+    
+    Y_pred = model.predict(X_test)
+    
+    mae = mean_absolute_error(Y_test, Y_pred)
+    mse = mean_squared_error(Y_test, Y_pred)
+    rmse = mse ** 0.5
+    r2 = r2_score(Y_test, Y_pred)
+    
+    rezultate.append([
+        file,
+        mae,
+        mse,
+        rmse,
+        r2
+    ])
+
+    df_results = pd.DataFrame(
+        rezultate,
+        columns=[
+            "Model",
+            "MAE",
+            "MSE",
+            "RMSE",
+            "R2"
+        ]
+    )
+    
+    print(f"Modelul {file}, a obtinut urmatoarele scoruri:\n Mean absolute error: {mae}\n Mean squared error: {mse}\n RMSE: {rmse}\n R2 Score: {r2}")
+
+print(df_results.round(2))
+
+
+
+
--- src/model_evaluation.py
+++ src/model_evaluation.py
@@ -1,6 +1,61 @@
 import pandas as pd
+import joblib
+from data_preprocessing import split_features_and_target
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
 DATA_PATH = "data/features_data.csv"
-MODEL_PATH = "models"
+MODEL_PATH = "models/car_price_LinearRegression.joblib"
 
-df = pd.read_csv(data)
+df = pd.read_csv(DATA_PATH)
+
+X, Y = split_features_and_target(df)
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+
+model = joblib.load(MODEL_PATH)
+
+Y_pred = model.predict(X_test)
+
+mae = mean_absolute_error(Y_test, Y_pred)
+mse = mean_squared_error(Y_test, Y_pred)
+rmse = mse ** 0.5
+r2 = r2_score(Y_test, Y_pred)
+
+print(Y_pred[:10])
+print(f"Mean absolute error: {mae}\n Mean squared error: {mse}\n RMSE: {rmse}\n R2 Score: {r2}")
+
+metrics = pd.DataFrame({
+    "metric": ["MAE", "MSE", "RMSE", "R2"],
+    "value": [mae, mse, rmse, r2],
+})
+ 
+print("\nRegression metrics:")
+print(metrics.round(2))
+
+print("\nCreating prediction analysis table...")
+ 
+prediction_analysis = pd.DataFrame({
+    "Actual Price": Y_test.values,
+    "Predicted Price": Y_pred,
+})
+
+prediction_analysis["Error Price"] = (
+    prediction_analysis["Actual Price"]
+    - prediction_analysis["Predicted Price"]
+)
+prediction_analysis["Absolute error price"] = (
+    prediction_analysis["Error Price"].abs()
+)
+print(
+    prediction_analysis
+    .sample(10, random_state=42)
+)
+
+print("\nLargest prediction errors:")
+ 
+print(
+    prediction_analysis
+    .sort_values("Absolute error price", ascending=False)
+    .head(10)
+)
